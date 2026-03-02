97b3203dd1cc98759ca0385bb17e2fc85e36711b
ja6285-bit <ja6285@srmist.edu.in>
2026-03-02 15:58:57 +0530

    USecase 2

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,7 +1,32 @@
-public class PalindromeChecker {
+import java.util.Scanner;
 
-  public static void main() {
-        System.out.println("Welcome to palindromechecker");
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user to enter a string
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        // Close scanner
+        sc.close();
     }
-
 }
